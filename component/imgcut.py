9d9c591357de76635eeaf90dd9b4b17084d06cfc
zs <810909753@qq.com>
2018-03-25 12:09:21 +0800

    324 版本

--- component/imgcut.py
+++ component/imgcut.py
@@ -327,7 +327,7 @@ def findrefresh():
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
     yanzhengma_find = get_val('yanzhengma_find')
     if max_val >= 0.8 and yanzhengma_find:
-        # OnClick_Shuaxin() #刷新验证码
+        OnClick_Shuaxin() #刷新验证码
         set_val('yanzhengma_view', True)  # 激活放大器
         set_val('yanzhengma_count', 0)  # 归零
         set_val('yanzhengma_find', False) #表示需要确认是否找到验证码
